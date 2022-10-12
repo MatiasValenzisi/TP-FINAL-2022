97c8f7a70770fc59e111f46c0cace5bcb91194ea
tobiimdp12 <tobiasselva@gmail.com>
2022-10-12 20:13:44 -0300

    mejorando estilo home

--- Views/dogView.php
+++ Views/dogView.php
@@ -33,10 +33,10 @@
                         <div class="form-group">
                             <label class="control-label col-md-3 col-sm-3 col-xs-12">Plan de vacunación: &nbsp;</label>
                             <div class="col-md-6 col-sm-6 col-xs-12">
-                                <img src="<?php echo VACCINATION.$this->dog->getVaccinationPlan() ?>"
-                                alt="" height="320" width="320">
+                                <img src="<?php echo VACCINATION.$this->dog->getVaccinationPlan() ?>" alt=""
+                                    height="320" width="320">
                             </div>
-                        </div>  
+                        </div>
 
                         <div class="form-group">
                             <label class="control-label col-md-3 col-sm-3 col-xs-12">Raza: &nbsp;</label>
@@ -68,41 +68,46 @@
                         <div class="form-group">
                             <label class="control-label col-md-3 col-sm-3 col-xs-12">Observaciones: &nbsp;</label>
                             <div class="col-md-6 col-sm-6 col-xs-12">
-                                <textarea class="form-control" style="resize:none;" readonly><?php echo $this->dog->getObservations() ?></textarea>
+                                <textarea class="form-control" style="resize:none;"
+                                    readonly><?php echo $this->dog->getObservations() ?></textarea>
                             </div>
                         </div>
 
                         <?php if (!is_null($this->dog->getPhoto()) && !empty($this->dog->getPhoto())){ ?>
 
-                        <br>   
+                        <br>
 
                         <div class="form-group">
                             <label class="control-label col-md-3 col-sm-3 col-xs-12">Foto: &nbsp;</label>
                             <div class="col-md-6 col-sm-6 col-xs-12">
-                                <img src="<?php echo PHOTO.$this->dog->getPhoto() ?>"
-                                alt="" height="320" width="320">
+                                <img src="<?php echo PHOTO.$this->dog->getPhoto() ?>" alt="" height="320" width="320">
                             </div>
-                        </div> 
+                        </div>
 
                         <?php } if (!is_null($this->dog->getVideo()) && !empty($this->dog->getVideo())){ ?>
 
-                        <br>    
+                        <br>
 
+                        <?php var_dump(VIDEO.$this->dog->getVideo())?>
                         <div class="form-group">
                             <label class="control-label col-md-3 col-sm-3 col-xs-12">Video: &nbsp;</label>
                             <div class="col-md-6 col-sm-6 col-xs-12">
-                                <video src="<?php echo VIDEO.$this->dog->getVideo() ?>" width=320 height=320 controls poster="">
+                                <video width=320 height=320 controls>
+                                    <source src="https://pixabay.com/es/videos/cachorro-perro-mascota-canino-1047/"
+                                        type="video/mp4">
                                     Lo sentimos. Este vídeo no puede ser reproducido en tu navegador.
                                 </video>
                             </div>
-                        </div> 
-                        
+                        </div>
+
                         <?php } ?>
 
                         <br>
 
-                        <div class="form-group">                            
-                            <button type="button" style="margin-left: 30%;" onclick="location.href='<?php echo FRONT_ROOT ?>/pet/list'" class="btn btn-primary">Volver al listado de mascotas</button>
+                        <div class=" form-group">
+                            <button type="button" style="margin-left: 30%;"
+                                onclick="location.href='<?php echo FRONT_ROOT ?>/pet/list'"
+                                class="btn btn-primary">Volver al listado de mascotas</button>
                         </div>
 
                     </form>
--- Views/homeView.php
+++ Views/homeView.php
@@ -1,43 +1,91 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+    .home-container {
+        margin-top: 10px;
+        width: 100%;
+
+        color: #1b2a3a;
+    }
+
+    .utn-logo {
+
+        position: absolute;
+        top: 0;
+        right: 0;
+        margin-right: 20px;
+        width: 150px;
+        height: 50px;
+    }
+
+    .title {
+        text-align: center;
+        font-size: 50px;
+        margin-bottom: 20px;
+        font-weight: 800;
+    }
+
+    .subtitle {
+        text-align: center;
+        font-size: 30px;
+        margin-bottom: 20px;
+        font-weight: 500;
+    }
+
+    p {
+        font-size: 25px;
+        font-weight: 400;
+    }
+
+    .images {
+        display: flex;
+        justify-content: space-evenly;
+        align-items: center;
+        flex-wrap: wrap;
+        margin: 50px;
+
+    }
+
+    .images img {
+        object-fit: cover;
+        width: 350px;
+        height: 350px;
+    }
+    </style>
 </head>
+
 <body>
-    <div style="margin-left: 70%;">
-        <img src="../Temporal-pruebas/utn.png">
+    <img src="../Temporal-pruebas/utn.png" alt="utn" class="utn-logo">
+    <div class="home-container">
+
+
+
+        <div>
+            <h1 class="title">Pet Hero</h1>
+            <h3 class="subtitle">Slogan</h3>
+        </div>
     </div>
 
-    <div style="margin-left: 45%;">
-        <h1>Pet Hero</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse, autem quidem quam atque sint assumenda Lorem
+        ipsum dolor sit amet consectetur, adipisicing elit. Fugit, voluptatum laudantium? Esse suscipit, dicta quibusdam
+        blanditiis vel debitis omnis, ad reprehenderit quas rerum, in quis odio aut numquam. Praesentium, eveniet?
+        repudiandae dolorem reiciendis sapiente, veritatis asperiores doloribus ducimus optio dolorum saepe qui eius
+        eaque id!</p>
+
+    <div class="images">
+        <img src="../Temporal-pruebas/home2.jpeg" alt="animal">
+        <img src="../Temporal-pruebas/home1.jpeg" alt="animal">
     </div>
 
-    <div style="margin-left: 20px; margin-right: 20px">
-        <br><h4>Se requiere realizar una aplicación cuyo modelo de negocio consiste en que personas puedan brindar el servicio del cuidado de mascotas. Dicho cuidado se trata de una estadía corta a cambio de una remuneración.</h4>
-        <h4>Los usuarios que se registren como Guardianes, tienen un perfil en el sitio donde exponen que tipo de perro están dispuestos a cuidar (pequeño, mediano o grande) y la remuneración esperada por la estadía.</h4>
-        <h4>Por otro lado, existe el tipo de usuario Dueño que registra un nuevo perfil en la aplicación y será quien contrate el servicio de los Guardianes. Una vez completado el alojamiento del perro, los Dueños tienen la habilidad de generar una review sobre el servicio. Estas reviews generarán en el Guardián una mayor reputación dentro de la aplicación</h4>
-        <h4>La aplicación les permite a los usuarios Guardianes, definir los días específicos que cuentan con disponibilidad para el cuidado de perros. Esta información será de utilidad para los Dueños al momento de reservar el servicio.</h4>
-        <h4>Con motivo de seguridad para las mascotas, un Guardián solamente puede cuidar a un perro por estadía.</h3>
-        <h4>Los Dueños deberán crearle un perfil a cada mascota que poseen. Por cada perfil de mascota, deben cargar: una foto, raza, tamaño, plan de vacunación (como imagen) y observaciones generales de la misma. La aplicación también brinda la oportunidad de subir un video del perro.</h4>
-        <h4>Cuando un Dueño selecciona un Guardián de su agrado, se generará una reserva en el sistema entre las fechas que requiere. El Guardián en cuestión, deberá aceptar o rechazar esta nueva reserva. </h4>
-        <h4>En caso de que la reserva sea aceptada por el Guardián, se envía un cupón de pago al Dueño con el 50% del costo del total de la estadía. Al momento de efectuar el pago, la reserva queda confirmada</h4>
-        
-        <br><h2><strong>Requisitos no funcionales:</strong></h2>
-        <h4>Programación en capas de la aplicación respetando la arquitectura de 3 capas lógicas vista durante la cursada. Esto implica el desarrollo de las clases que representen las entidades del modelo y controladoras de los casos de uso, las vistas y la capa de acceso a datos.</h4>
-        
-        <br><h2><strong>Implementacion para la aprobacion</strong></h3>
-        <br><h2><strong>Primera revision:</strong></h2>
-        <br><h4>RF1 - Ingresando nuevo Dueño en la aplicación.</h4>
-        <h4>RF2 - Ingresando nueva mascota en la aplicación.</h4>
-        <h4>RF3 - Consultando mi listado de mascotas (Dueño).</h4>
-        <h4>RF4 - Ingresar nuevo Guardián.</h4>
-        <h4>RF5 - Un Guardián podrá indicar la disponibilidad de estadías.</h4>
-        <h4>RF6 - Consultar listado de Guardián cómo Dueño.</h4>
-   
-   
-    </div>
+
+    <h4 class="title">Contactanos!</h4>
+
 </body>
+
 </html>

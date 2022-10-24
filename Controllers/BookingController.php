4986341624054f086a3b081a370155b9a135c5ee
MatiasValenzisi <91572020+MatiasValenzisi@users.noreply.github.com>
2022-10-23 21:52:27 -0300

    Primera parte de consultar reserva

--- Controllers/BookingController.php
+++ Controllers/BookingController.php
@@ -4,6 +4,8 @@
     use DAO\BookingDAO as BookingDAO;
     use DAO\GuardianDAO as GuardianDAO;
     use DAO\OwnerDAO as OwnerDAO;
+    use DAO\DogDAO as DogDAO;
+    use DAO\CatDAO as CatDAO;
 
     class BookingController {
 
@@ -13,17 +15,99 @@
         private $guardianDAO;
         private $owner;
         private $ownerDAO;
+        private $pet;
+        private $petList;
+        private $dogDAO;
+        private $catDAO;
 
         public function __construct(){
 
-            $this->bookingDAO = new BookingDAO();
+            $this->bookingList = array();
+            $this->bookingDAO  = new BookingDAO();
+            $this->guardian    = null;
             $this->guardianDAO = new GuardianDAO();
-            $this->ownerDAO = new OwnerDAO();
+            $this->owner       = null;
+            $this->ownerDAO    = new OwnerDAO();
+            $this->pet         = null;
+            $this->petList     = array();
+            $this->dogDAO      = new DogDAO();            
+            $this->catDAO      = new CatDAO();
         }
 
-        public function create($tokenGuardian = null, $type = null, $action = null, $specific = null){
+        public function consult($tokenGuardian = null, $type = null, $action = null, $specific = null){
 
-            echo "Crear reserva...";
+            $date = array();
+            $date = date("m-d-Y").' - '.date("m-d-Y");
+
+            $this->guardian = $this->guardianDAO->getUserTokenDAO($tokenGuardian);
+
+            $nameGuardian   = $this->guardian->getFirstName()." ".$this->guardian->getLastName();
+            $nameOwner      = $_SESSION['userPH']->getFirstName()." ".$_SESSION['userPH']->getLastName();
+
+            $dogList = $this->dogDAO->getAllDAO();
+            $catList = $this->catDAO->getAllDAO();
+            
+            if (!empty($dogList)){
+
+                $this->petList = array_merge($this->petList, $dogList);
+            } 
+
+            if (!empty($catList)){
+
+                $this->petList = array_merge($this->petList, $catList);
+            }     
+
+            require_once ROOT_VIEWS."/mainHeader.php";
+            require_once ROOT_VIEWS."/mainNav.php";
+            require_once ROOT_VIEWS."/ownerBookingConsultView.php";    
+            require_once ROOT_VIEWS."/notificationAlert.php"; 
+            require_once ROOT_VIEWS."/mainFooter.php";
+        }
+
+        public function consultAction($tokenGuardian, $tokenPet, $reservation){
+
+            $dataNew   = explode(" - ", $reservation);
+
+            $startDate = date("m/d/Y", strtotime($dataNew["0"]));
+            $startDate = date("Y-m-d", strtotime($startDate));
+
+            $endDate   = date("m/d/Y", strtotime($dataNew["1"]));
+            $endDate   = date("Y-m-d", strtotime($endDate));
+
+            if ($this->guardianList = $this->verifyDate($tokenGuardian, $startDate, $endDate)){
+
+                $dateMin = date("Y-m-d", strtotime("+ 7 days")); 
+
+                if ($startDate >= $dateMin){
+
+                    header("Location: ".FRONT_ROOT."/booking/generate/".$tokenGuardian."/".$tokenPet."/".$startDate."/".$endDate);                
+
+                } else {
+
+                    header("Location: ".FRONT_ROOT."/booking/consult/".$tokenGuardian."/generate/error/verify/date");
+                } 
+
+            } else {
+
+                header("Location: ".FRONT_ROOT."/booking/consult/".$tokenGuardian."/generate/error/verify/guardian");
+            }  
+        }
+
+        // Verificar si en el rango de fechas, trabaja el guardian seleccionado.
+
+        private function verifyDate($tokenGuardian, $startDate, $endDate){
+
+            return true;
+        }
+
+        public function generate($tokenGuardian, $tokenPet, $startDate, $endDate){
+
+            echo "Confirmar realización de reserva...";
+        }
+
+        public function generateAction(){
+
+            echo "Crear realizacion de reserva...";
         }
 
         public function list($typeUser = null) { 
@@ -38,14 +122,14 @@
                 $this->guardian = $this->guardianDAO->getUserNameDAO($_SESSION['userPH']->getToken());
                 $this->bookingList = $this->guardian->getBookingList();
 
-                require_once ROOT_VIEWS."/guardianBookingList.php";   
+                require_once ROOT_VIEWS."/guardianBookingListView.php";   
 
             } else if(strcmp(get_class($_SESSION['userPH']), "Models\Owner") == 0) {
 
                 $this->owner = $this->ownerDAO->getUserNameDAO($_SESSION['userPH']->getToken());
                 $this->bookingList = $this->owner->getBookingList();
 
-                require_once ROOT_VIEWS."/ownerBookingList.php";   
+                require_once ROOT_VIEWS."/ownerBookingListView.php";   
             }
 
             require_once ROOT_VIEWS."/mainNav.php";  
---
+++ Views/ownerBookingConsultView.php
@@ -0,0 +1,77 @@
+        <div>
+            <div class="clearfix"></div>            
+            <div class="row">
+                <div class="col-md-12 col-sm-12 col-xs-12">
+                    <div class="x_panel">
+                        <div class="x_title">
+                            <h2>Consultar reserva</h2>
+                            <div class="clearfix"></div>
+                        </div>
+
+                        <div class="x_content"><br>
+
+                        <form class="form-horizontal form-label-left" action="<?php echo FRONT_ROOT?>/booking/consultAction" method="POST">
+
+                            <input type="hidden" name="tokenGuardian" value="<?php echo $tokenGuardian ?>">
+
+                            <div class="form-group">
+                                <label class="control-label col-md-3 col-sm-3 col-xs-12">Nombre de guardian &nbsp;</label>
+                                <div class="col-md-6 col-sm-6 col-xs-12">
+                                    <label class="form-control col-md-7 col-xs-12" style="font-weight: normal;"><?php echo $nameGuardian; ?></label>
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label class="control-label col-md-3 col-sm-3 col-xs-12">Nombre del usuario &nbsp;</label>
+                                <div class="col-md-6 col-sm-6 col-xs-12">
+                                    <label class="form-control col-md-7 col-xs-12" style="font-weight: normal;"><?php echo $nameOwner; ?></label>
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label class="control-label col-md-3 col-sm-3 col-xs-12" for="tokenPet">Seleccionar mascota <span class="required">*</span></label>
+                                <div class="col-md-6 col-sm-6 col-xs-12">
+                                    <select class="form-control" id="tokenPet" name="tokenPet" required>
+
+                                        <option value="">Seleccionar mascota para la reserva</option>
+
+                                        <?php foreach ($this->petList as $key => $pet) { ?>
+                                           
+                                            <option value="<?php echo $pet->getToken() ?>"><?php echo $pet->getName()." - ".$pet->getRace() ?></option>
+
+                                        <?php } ?>
+                                        
+                                    </select>
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label class="control-label col-md-3 col-sm-3 col-xs-12" for="reservation">Fecha rango de reserva: <span class="required">*</span></label>
+                                <div class="col-md-6 col-sm-6 col-xs-12">
+                                    <fieldset>
+                                        <div class="control-group">
+                                            <div class="controls">
+                                                <div class="input-prepend input-group">
+                                                    <span class="add-on input-group-addon"><i class="glyphicon glyphicon-calendar fa fa-calendar"></i></span>
+                                                    <input type="text" class="form-control col-md-7 col-xs-12" name="serviceDate" id="reservation" class="form-control" value="<?php echo $date ?>" />
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </fieldset>
+                                </div>
+                            </div>
+
+                            <div class="ln_solid"></div>
+                            <div class="form-group">
+                                <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">                                          
+                                    <button class="btn btn-primary" type="button" onclick="location.href='<?php echo FRONT_ROOT ?>/guardian/view/<?php echo $tokenGuardian; ?>'" style="width:40%;">Cancelar reserva</button>
+                                    <button type="submit" class="btn btn-success" style="width:40%;">Realizar consulta</button>
+                                </div>
+                            </div>
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>

02b6bb75d72dece6a0792ea4bcd04f737ca7a869
MatiasValenzisi <91572020+MatiasValenzisi@users.noreply.github.com>
2022-10-28 14:22:30 -0300

    Mascotas con foto obligatoria

--- Views/catCreateView.php
+++ Views/catCreateView.php
@@ -87,7 +87,7 @@
                             <label class="control-label col-md-3 col-sm-3 col-xs-12" for="photo">Foto <span
                                     class="required">*</span></label>
                             <div class="col-md-6 col-sm-6 col-xs-12">
-                                <input type="file" class="form-control col-md-7 col-xs-12" name="photo" id="photo" />
+                                <input type="file" class="form-control col-md-7 col-xs-12" name="photo" id="photo" required/>
                             </div>
                         </div>
 

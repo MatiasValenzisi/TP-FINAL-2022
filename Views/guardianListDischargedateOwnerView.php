7a00813bff671be28e92034f00372b73cb1f65bd
nicolasbrun333 <nicolasbrun333@gmail.com>
2022-11-14 13:20:31 -0300

    Cambio menor estrellas review

--- Views/guardianListDischargedateOwnerView.php
+++ Views/guardianListDischargedateOwnerView.php
@@ -113,58 +113,23 @@
                                 <div class="col-xs-12 col-sm-6 emphasis">
                                     <p class="ratings">
                                         <?php
-                                            if($this->getReviewAverage($guardian->getToken()) != 0) { ?>
-                                                <a><?php echo $this->getReviewAverage($guardian->getToken()); ?></a>
-                                            <?php
-                                            } ?>
+                                        $average = $this->getReviewAverage($guardian->getToken());
 
-                                        <?php 
-                                        switch($this->getReviewAverage($guardian->getToken())) {
-                                            case 1: ?>
-                                                <a><span class="fa fa-star"></span></a>
-                                                <a><span class="fa fa-star-o"></span></a>
-                                                <a><span class="fa fa-star-o"></span></a>
-                                                <a><span class="fa fa-star-o"></span></a>
-                                                <a><span class="fa fa-star-o"></span></a>
-                                                <?php
-                                                break;
-                                            case 2: ?>
-                                                <a><span class="fa fa-star"></span></a>
-                                                <a><span class="fa fa-star"></span></a>
-                                                <a><span class="fa fa-star-o"></span></a>
-                                                <a><span class="fa fa-star-o"></span></a>
-                                                <a><span class="fa fa-star-o"></span></a>
-                                                <?php
-                                                break;
-                                            case 3: ?>
-                                                <a><span class="fa fa-star"></span></a>
-                                                <a><span class="fa fa-star"></span></a>
-                                                <a><span class="fa fa-star"></span></a>
-                                                <a><span class="fa fa-star-o"></span></a>
-                                                <a><span class="fa fa-star-o"></span></a>
-                                                <?php
-                                                break;
-                                            case 4: ?>
-                                                <a><span class="fa fa-star"></span></a>
-                                                <a><span class="fa fa-star"></span></a>
-                                                <a><span class="fa fa-star"></span></a>
-                                                <a><span class="fa fa-star"></span></a>
-                                                <a><span class="fa fa-star-o"></span></a>
-                                                <?php
-                                                break;
-                                            case 5: ?>
-                                                <a><span class="fa fa-star"></span></a>
-                                                <a><span class="fa fa-star"></span></a>
-                                                <a><span class="fa fa-star"></span></a>
-                                                <a><span class="fa fa-star"></span></a>
-                                                <a><span class="fa fa-star"></span></a>
-                                                <?php
-                                                break;
-                                            default:?>
-                                                No disponible
-                                                <?php
-                                                break;
-                                            
+                                        if($average != 0) { ?>
+                                            <a><?php echo $average; ?></a>
+                                        <?php
+                                        } 
+
+                                        if($average == 0) {
+                                            echo "No disponible";
+                                        } else {
+                                            for($i = 0; $i < 5; $i++) {
+                                                if($i < ($average)) { ?>
+                                                    <a><span class="fa fa-star"></span></a> <?php 
+                                                } else { ?>
+                                                    <a><span class="fa fa-star-o"></span></a> <?php
+                                                }
+                                            }
                                         }
                                         ?>
 

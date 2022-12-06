3ce9645865480ae9de9d8c52c567a0d6cc6de91d
MatiasValenzisi <91572020+MatiasValenzisi@users.noreply.github.com>
2022-12-06 18:02:22 -0300

    Se limpia home

--- Views/homeView.php
+++ Views/homeView.php
@@ -1,58 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
+    
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-
-</head>
-
-<body>
-
-    <div class="home-container x_panel">
-
-
-
-        <div>
-            <h1 class="title x_title">Pet Hero</h1>
-            <div class="clearfix"></div>
-            <h3 class="subtitle ">Slogan</h3>
-        </div>
-
-
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse, autem quidem quam atque sint assumenda Lorem
-            ipsum dolor sit amet consectetur, adipisicing elit. Fugit, voluptatum laudantium? Esse suscipit, dicta
-            quibusdam
-            blanditiis vel debitis omnis, ad reprehenderit quas rerum, in quis odio aut numquam. Praesentium, eveniet?
-            repudiandae dolorem reiciendis sapiente, veritatis asperiores doloribus ducimus optio dolorum saepe qui eius
-            eaque id!</p>
-
-        <div class="images">
-            <img src="../Temporal-pruebas/home2.jpeg" alt="animal">
-            <img src="../Temporal-pruebas/home1.jpeg" alt="animal">
-        </div>
-        <h2 class="title">Integrantes</h2>
-        <div class="members">
-
-            <div class="member">
-                <img src="../Views/profile/tobias.jpg" alt="tobias">
-                <h4>Tobias</h4>
-            </div>
-            <div class="member">
-                <img src="../Views/profile/matias.jpg" alt="matias">
-                <h4>Matias</h4>
-            </div>
-            <div class="member">
-                <img src="../Views/profile/nicolas.jpg" alt="nicolas">
-                <h4>Nicolas</h4>
-            </div>
-        </div>
-
-
+    <div>
+        <h1 class="title x_title">Pet Hero</h1>
+        <div class="clearfix"></div>
+        <h3 class="subtitle ">Slogan</h3>
     </div>
-    </div>
-</body>
-
-</html>
+        
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse, autem quidem quam atque sint assumenda Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit, voluptatum laudantium? Esse suscipit, dicta quibusdam blanditiis vel debitis omnis, ad reprehenderit quas rerum, in quis odio aut numquam. Praesentium, eveniet? repudiandae dolorem reiciendis sapiente, veritatis asperiores doloribus ducimus optio dolorum saepe qui eius eaque id!</p>

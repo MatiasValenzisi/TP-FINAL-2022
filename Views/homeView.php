76a0a32d66a83ccd878a5c76d40781a87193f1b1
MatiasValenzisi <91572020+MatiasValenzisi@users.noreply.github.com>
2022-12-19 14:22:49 -0300

    Home view

--- Views/homeView.php
+++ Views/homeView.php
@@ -1,17 +1,14 @@
     <div>
         <h1 class="title x_title">Pet Hero</h1>
-        <div class="clearfix"></div>
-        <h3 class="subtitle ">Slogan</h3>
     </div>
-        
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse, autem quidem quam atque sint assumenda Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit, voluptatum laudantium? Esse suscipit, dicta quibusdam blanditiis vel debitis omnis, ad reprehenderit quas rerum, in quis odio aut numquam. Praesentium, eveniet? repudiandae dolorem reiciendis sapiente, veritatis asperiores doloribus ducimus optio dolorum saepe qui eius eaque id!</p>
-
-
+    
     <div class="gallery-home">
-        <img src="<?php echo FRONT_ROOT ?>/Temporal-pruebas/Home/chihuahua.jpeg" alt="dog">
-        <img src="<?php echo FRONT_ROOT ?>/Temporal-pruebas/Home/cat-dog.jpeg" alt="dog">
-        <img src="<?php echo FRONT_ROOT ?>/Temporal-pruebas/Home/dep.jpeg" alt="dog">
-        <img src="<?php echo FRONT_ROOT ?>/Temporal-pruebas/Home/dog1.jpeg" alt="dog">
-        <img src="<?php echo FRONT_ROOT ?>/Temporal-pruebas/Home/nefhdjk.jpeg" alt="dog">
-        <img src="<?php echo FRONT_ROOT ?>/Temporal-pruebas/Home/perros.jpeg" alt="dog">
+        <img src="<?php echo VIEWS_PATH ?>/home/1.jpg" alt="dog">
+        <img src="<?php echo VIEWS_PATH ?>/home/2.jpg" alt="dog">
+        <img src="<?php echo VIEWS_PATH ?>/home/3.jpg" alt="dog">
+        <img src="<?php echo VIEWS_PATH ?>/home/4.jpg" alt="dog">
+        <img src="<?php echo VIEWS_PATH ?>/home/5.jpg" alt="dog">
+        <img src="<?php echo VIEWS_PATH ?>/home/6.jpg" alt="dog">
+        <img src="<?php echo VIEWS_PATH ?>/home/7.jpg" alt="dog">
+        <img src="<?php echo VIEWS_PATH ?>/home/8.jpg" alt="dog">        
     </div>
